600bb082b13869bb589fb080f70f1ce783fcc14b
Igor Korkin <igor.korkin@gmail.com>
2018-02-28 18:37:07 +0300

    the first perceptron and epochs

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -58,3 +58,30 @@ class Perceptron(object):
     def predict(self, X):
         """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+def main():
+    # load data
+    import pandas as pd
+    url = 'K:\Dropbox\AI\code\iris.data.txt'
+    df = pd.read_csv(url, header=None)
+    import matplotlib.pyplot as plt
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0, 2]].values
+    plt.scatter(X[:50, 0], X[:50, 1],
+    color='red', marker='o', label='setosa')
+    plt.scatter(X[50:100, 0], X[50:100, 1],
+    color='blue', marker='x', label='versicolor')
+    plt.xlabel('petal length')
+    plt.ylabel('sepal length')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    ppn = Perceptron(eta=0.1, n_iter=10)
+    ppn.fit(X, y)
+    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_,marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel('Number of misclassifications')
+    plt.show()
+
+main() 
